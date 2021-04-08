ca01f866243517d28e93feb62f93e61ec897a102
jiyunjeong0229 <64758378+jiyunjeong0229@users.noreply.github.com>
2021-04-08 21:39:26 +0900

    Add files via upload

---
+++ mergetest.js
@@ -0,0 +1,17 @@
+let adder = function (x, y) {
+    return x + y;
+  };
+  let divide = function (x, y) {
+    return x / y;
+  };
+  let multiply = function (x, y) {
+    return x * y;
+  };
+  function remainder (x, y) {
+    return x % y;
+  };
+  function addminus (x, y) {
+    return (x + x) - y;
+  };
+  
+  
